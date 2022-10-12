1eb668ee146670c265b325de95e06a728b9c788b
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-12 20:45:33 +0300

    feat: add solution ninth task

--- unit_01/unit_01.js
+++ unit_01/unit_01.js
@@ -115,7 +115,7 @@ document.querySelector('.b-8').onclick = f8;
 // Усложним предыдущие задачи. С помощью цикла повесьте на блоки out-9 событие клик. По клику должна выполняться функция f9. Функция, должна добавлять класс bg-9 тому out-9 на котором кликнули.
 
 function f9() {
-	//this.classList...  // все решается одной строкой
+	this.classList.toggle('bg-9')
 }
 
 let div9 = document.querySelectorAll('.out-9');
