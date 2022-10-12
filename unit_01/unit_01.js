f7a6779e61edd21ac0aa66a5c3d65082aa059a88
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-12 20:40:00 +0300

    feat: add solution fifth task

--- unit_01/unit_01.js
+++ unit_01/unit_01.js
@@ -59,11 +59,9 @@ document.querySelector('.b-4').onclick = f4;
 // По нажатию b-5 запускайте функцию f5, которая проверяет наличие класса bg-4 у блока out-4 (да, именно bg-4 у out-4 ). Результат - true или false, выводите в out-5.
 
 function f5() {
-
 	const out = document.querySelector('.out-5');
 
 	out.textContent = document.querySelector('.out-4').classList.contains('bg-4');
-
 }
 
 document.querySelector('.b-5').onclick = f5;
