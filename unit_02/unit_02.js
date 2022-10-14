27e3bd9be11b7c6a136a88baba6e6cb39edcae90
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-14 15:21:39 +0300

    feat: add solution task 5

--- unit_02/unit_02.js
+++ unit_02/unit_02.js
@@ -68,9 +68,14 @@ document.querySelector('.b-4').onclick = f4;
 // Вывод в out-5
 // Тест допустим массив [1,2,3,4,5,6,7,8,9,10] вывод: 8
 
-// let ar5 =
+let ar5 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 function f5() {
+
+    const out = document.querySelector('.out-5');
+
+    out.innerHTML = ar5[0] + ar5[2] + ar5[3];
+
 }
 
 document.querySelector('.b-5').onclick = f5;
