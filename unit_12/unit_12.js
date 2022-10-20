53574f7dfadde780f7b1e5df4a3318f1d62c95d1
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-20 14:49:11 +0300

    feat: add solution task 7

--- unit_12/unit_12.js
+++ unit_12/unit_12.js
@@ -97,10 +97,12 @@ div4.addEventListener('touchend', t6)
 
 function t7() {
 
+	this.style.backgroundColor = 'red';
+
 }
 
 // ваше событие здесь!!!
-
+document.querySelector('.div-7').addEventListener('touchmove', t7);
 // Task 8 ============================================
 /*  Дан блок div-8. Добавьте на него событие touch, которое при срабатывании окрашивает блок случаным цветом из массива a8=[red, green, blue, orange, pink, yellow] */
 
